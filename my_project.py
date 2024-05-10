f023c3daf5710eeda8a54e81721b9e9e036d40dd
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-05-10 19:45:08 -0400

    changes made in the marimo GUI

--- my_project.py
+++ my_project.py
@@ -1,7 +1,7 @@
 import marimo
 
 __generated_with = "0.5.2"
-app = marimo.App()
+app = marimo.App(width="full", app_title="Some Project of Mine")
 
 
 @app.cell
@@ -9,8 +9,24 @@ def __():
     import json
     import requests
     import pandas
-
     return json, pandas, requests
+
+
+@app.cell
+def __(pandas):
+    d = pandas.DataFrame([1, 2, 3, 4])
+    return d,
+
+
+@app.cell
+def __(d):
+    d
+    return
+
+
+@app.cell
+def __():
+    return
 
 
 if __name__ == "__main__":
